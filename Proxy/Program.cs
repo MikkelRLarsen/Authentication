681cc1259e8153f1deddec92195f216585cac346
Mikkel Schmidt Nielsen <mikkelschmidt@msn.com>
2025-10-06 18:51:11 +0200

    Made RateLimiting

--- Proxy/Program.cs
+++ Proxy/Program.cs
@@ -1,3 +1,4 @@
+using System.Threading.RateLimiting;
 using Yarp.ReverseProxy;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,9 +6,26 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddPolicy("LoginPolicy", context =>
+        RateLimitPartition.GetFixedWindowLimiter(
+            partitionKey: context.Connection.RemoteIpAddress?.ToString() ?? "unknown",
+            factory: key => new FixedWindowRateLimiterOptions
+            {
+                PermitLimit = 3,
+                Window = TimeSpan.FromSeconds(10),
+                QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+                QueueLimit = 2
+            }));
+});
+
 var app = builder.Build();
 
+app.UseRateLimiter();
+
 // Map YARP
-app.MapReverseProxy();
+app.MapReverseProxy()
+    .RequireRateLimiting("LoginPolicy");
 
 app.Run();
