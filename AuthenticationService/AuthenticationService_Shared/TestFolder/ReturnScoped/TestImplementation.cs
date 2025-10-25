46a0e3312107ef85e435781b01a7b8b10fb4574d
MikkelRLarsen <Thenuker95@gmail.com>
2025-10-26 00:14:13 +0200

    Created Test to show IHttpClientFactory

--- AuthenticationService/AuthenticationService_Shared/TestFolder/ReturnScoped/TestImplementation.cs
+++ AuthenticationService/AuthenticationService_Shared/TestFolder/ReturnScoped/TestImplementation.cs
@@ -8,21 +8,25 @@ namespace AuthenticationService_Shared.TestFolder.ReturnScoped
 		private readonly string _randomStringAsANumber;
 		private readonly ITestInterfaceSingleton _iTestInterfaceSingleton;
 		private readonly IConfigurationDictonary _iConfigurationDictonary;
-		public TestImplementation(ITestInterfaceSingleton iTestInterfaceSingleton, IConfigurationDictonary configurationDictonary)
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public TestImplementation(ITestInterfaceSingleton iTestInterfaceSingleton, IConfigurationDictonary configurationDictonary, IHttpClientFactory httpClientFactory)
 		{
 			Random rnd = new();
 			_randomStringAsANumber = rnd.Next(1, int.MaxValue).ToString();
 
 			_iTestInterfaceSingleton = iTestInterfaceSingleton;
 			_iConfigurationDictonary = configurationDictonary;
+			_httpClientFactory = httpClientFactory;
 		}
 
 		public string ReturnARandomString()
 		{
 			string singletonString = _iTestInterfaceSingleton.ReturnAString();
 			string configurationString = _iConfigurationDictonary.GetValue("RandomString");
+			string httpClientAdressString = _httpClientFactory.CreateClient("AuthService").BaseAddress.ToString();
 
-			return $"{_randomStringAsANumber} as a Scoped & {singletonString} as a Singleton & {configurationString}";
+			return $"{_randomStringAsANumber} as a Scoped & {singletonString} as a Singleton & {configurationString} & {httpClientAdressString} from HttpClient";
 		}
 	}
 }
